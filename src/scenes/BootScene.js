bf005e25a8b90c79da131abf361a097ed3a13e58
DG Costa <costadiegus@gmail.com>
2026-04-20 17:46:38 -0300

    Nova estrutura totalmente funcional

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -32,28 +32,27 @@ export default class BootScene extends Phaser.Scene {
 
     // --- Carregamento Centralizado de Assets ---
     // Imagens e Sprites
-    this.load.image('fireboy-idle', 'assets/fireboy.png');
-    this.load.image('watergirl-idle', 'assets/watergirl.png');
-    this.load.image('fireboy-jump', 'assets/fireboy-jump.png');
-    this.load.image('watergirl-jump', 'assets/watergirl-jump.png');
-    this.load.spritesheet('fireboy-walk', 'assets/fireboy-walk.png', { frameWidth: 64, frameHeight: 64 });
-    this.load.spritesheet('watergirl-walk', 'assets/watergirl-walk.png', { frameWidth: 64, frameHeight: 64 });
-    this.load.image('door-red', 'assets/door-red.png');
-    this.load.image('door-blue', 'assets/door-blue.png');
+    this.load.image('fireboy-idle', './src/assets/fireboy.png');
+    this.load.image('watergirl-idle', './src/assets/watergirl.png');
+    this.load.image('fireboy-jump', './src/assets/fireboy-jump.png');
+    this.load.image('watergirl-jump', './src/assets/watergirl-jump.png');
+    this.load.spritesheet('fireboy-walk', './src/assets/fireboy-walk.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('watergirl-walk', './src/assets/watergirl-walk.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.image('door-red', './src/assets/door-red.png');
+    this.load.image('door-blue', './src/assets/door-blue.png');
 
     // Áudios (Carregar tudo aqui garante que o som estará pronto no Menu)
-    this.load.audio('backgroundMusic', 'assets/background-music.mp3');
-    this.load.audio('gameOverMusic', 'assets/game-over-music.mp3');
-    this.load.audio('levelSceneMusic', 'assets/level-scene-music.mp3');
-    this.load.audio('click', 'assets/click.mp3');
-    this.load.audio('death', 'assets/death.mp3');
-    this.load.audio('jump', 'assets/jump.mp3');
-    this.load.audio('jump-fire', 'assets/jump-fire.mp3');
-    this.load.audio('jump-water', 'assets/jump-water.mp3');
+    this.load.audio('backgroundMusic', './src/assets/background-music.mp3');
+    this.load.audio('gameOverMusic', './src/assets/game-over-music.mp3');
+    this.load.audio('levelSceneMusic', './src/assets/level-scene-music.mp3');
+    this.load.audio('click', './src/assets/click.mp3');
+    this.load.audio('death', './src/assets/death.mp3');
+    this.load.audio('jump', './src/assets/jump.mp3');
+    this.load.audio('jump-fire', './src/assets/jump-fire.mp3');
+    this.load.audio('jump-water', './src/assets/jump-water.mp3');
   }
 
   create() {
-    this.game.audioManager = new AudioManager(this.game);
     this.game.audioManager.setMusicVolume(1);
     this.scene.start('MenuScene');
   }
